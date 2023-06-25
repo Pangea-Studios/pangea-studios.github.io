ecfc5fc5252f97305213e7f57320bd9ba0518067
Noinkin <87577662+Noinkin@users.noreply.github.com>
2023-06-25 18:25:18 +0100

    scroll anim

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener(
+    'scroll',
+    () => {
+        document.body.style.setProperty(
+            '--scroll',
+            window.scrollY /
+                (document.body.offsetHeight - window.innerHeight),
+        );
+
+		if (window.scrollY /
+			(document.body.offsetHeight - window.innerHeight) === 1) {
+			document.body.style.setProperty(
+				'--scroll',
+				0.9999,
+			);
+				}
+    },
+    false,
+);
