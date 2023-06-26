64b046ae3eb2a866e102f19d51c9fff9737f89ad
PokyBread <luksel09@gmail.com>
2023-06-26 18:37:27 +0100

    Added navbar and back to top button

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ window.addEventListener(
     },
     false,
 );
+
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
+  // Show/hide and animate the button based on scroll position
+  window.onscroll = function() {
+    var button = document.querySelector('.back-to-top');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      button.style.display = 'block';
+      button.style.bottom = '5vh'; // Position the button above the bottom of the screen
+    } else {
+      button.style.bottom = '-60vh'; // Position the button below the screen
+    }
+  }
